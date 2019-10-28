45419f993e323f7fd817ee8d65ec923d01c3c16d
Nico <none@none>
2019-10-28 20:27:02 +0100

    + Forget

--- Projects/Entries/2014/3/6_Little_Cells_files/6_Little_Cells.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20160418
-
-setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('6_Little_Cells_files/6_Little_CellsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupAllIEPNGBGs();BlogFixupPreviousNext();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
